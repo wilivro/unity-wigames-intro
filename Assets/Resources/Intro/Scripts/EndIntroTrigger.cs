2d14e92322be4ebf874fbdf9f8486bce53f32f90
kalmik <sergio.kalmik@gmail.com>
2016-03-23 14:43:04 -0300

    FirstCommit

---
+++ Assets/Resources/Intro/Scripts/EndIntroTrigger.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EndIntroTrigger : MonoBehaviour {
+
+	public void End() {
+		SendMessageUpwards("LoadLevel");
+	}
+}
---
+++ Assets/Resources/Intro/Scripts/Intro.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Intro : MonoBehaviour {
+
+	public string levelToLoad;
+	public void LoadLevel(){
+		try {
+			Application.LoadLevel(levelToLoad);
+		} catch {
+			Debug.Log("Invalid Level, please check if this level in built sistem");
+		}
+	}
+}
